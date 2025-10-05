0d6bbac7ebce80cd90dcb5b1e9cfebd62bea82d1
usltqwe <useletqwe@gmail.com>
2025-10-05 20:13:26 +0700

    Добавил form request

---
+++ app/Http/Requests/ItemEditRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ItemEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'item_name' => 'required|string|max:255',
+            'item_id' => 'required|string',
+            'item_icon' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ];
+    }
+}
